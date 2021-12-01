e778702d6d8e1fa139b9ffb5a6defd7fe1dd2e09
emattia <emattia4495@gmail.com>
2021-12-01 13:20:07 -0600

    try STATUS_FAIL

--- examples/simplest_case_with_status.py
+++ examples/simplest_case_with_status.py
@@ -1,8 +1,8 @@
 # https://github.com/hyperopt/hyperopt/wiki/FMin#12-attaching-extra-information-via-the-trials-object
-from hyperopt import fmin, tpe, hp, STATUS_OK
+from hyperopt import fmin, tpe, hp, STATUS_OK, STATUS_FAIL
 
 def objective(x):
-    return {'loss': x ** 2, 'status': STATUS_OK }
+    return {'loss': x ** 2, 'status': STATUS_FAIL if False else STATUS_OK}
 
 best = fmin(objective,
     space=hp.uniform('x', -10, 10),
