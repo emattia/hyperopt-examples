fa8d31f064d55792b1b107afb8dac10e449a223f
emattia <eddie.mattia@intel.com>
2021-12-01 13:11:37 -0600

    status example

---
+++ examples/simplest_case_with_status.py
@@ -0,0 +1,12 @@
+# https://github.com/hyperopt/hyperopt/wiki/FMin#12-attaching-extra-information-via-the-trials-object
+from hyperopt import fmin, tpe, hp, STATUS_OK
+
+def objective(x):
+    return {'loss': x ** 2, 'status': STATUS_OK }
+
+best = fmin(objective,
+    space=hp.uniform('x', -10, 10),
+    algo=tpe.suggest,
+    max_evals=100)
+
+print(best)
